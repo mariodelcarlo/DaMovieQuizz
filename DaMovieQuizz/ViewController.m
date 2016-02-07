a6f13aabf3055f1d51f7a6fa66d747027bcc35fb
Del Carlo Marie-Odile <marie-odile.delcarlo@MacBook-Pro-de-Del.local>
2016-02-07 18:32:06 +0100

    Initial commit

---
+++ DaMovieQuizz/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DaMovieQuizz
+//
+//  Created by Del Carlo Marie-Odile on 07/02/2016.
+//  Copyright © 2016 Del Carlo Marie-Odile. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ DaMovieQuizz/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DaMovieQuizz
+//
+//  Created by Del Carlo Marie-Odile on 07/02/2016.
+//  Copyright © 2016 Del Carlo Marie-Odile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
