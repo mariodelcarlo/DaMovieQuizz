c8b82b667230de7d926d5b9b5e4724f52d3bc8ce
Marie-Odile Del Carlo <marieodile.delcarlo@gmail.com>
2016-02-09 22:33:26 +0100

    Add GameSummaryViewController, class and in storyboard

---
+++ DaMovieQuizz/GameSummaryViewController.h
@@ -0,0 +1,13 @@
+//
+//  GameSummaryViewController.h
+//  DaMovieQuizz
+//
+//  Created by Del Carlo Marie-Odile on 09/02/2016.
+//  Copyright © 2016 Del Carlo Marie-Odile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameSummaryViewController : UIViewController
+
+@end
